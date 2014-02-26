c22797d69f9a21b5c8d9b289dba6a8da9ad6d104
supermomonga <hi@supermomonga.com>
2014-02-27 04:46:18 +0900

    hi

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y git zsh
 curl https://raw.github.com/sindresorhus/pure/master/pure.zsh > pure.zsh
 sudo ln -s "$PWD/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
 
-git clone git@github.com:supermomonga/minimal-dotfiles.git dotfiles
+git clone https://github.com/supermomonga/minimal-dotfiles.git dotfiles
 
 ln -s ./dotfiles/.zshrc ./.zshrc
 ln -s ./dotfiles/.vimrc ./.vimrc
