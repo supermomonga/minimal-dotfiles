0bd0fceddadaabe6f77ad3c399a960e04ab9e596
supermomonga <hi@supermomonga.com>
2014-03-20 01:36:08 +0900

    vim config

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get upgrade
 
 echo "\n\n"
 echo "----------> Install common packages"
-sudo apt-get install -y git zsh
+sudo apt-get install -y git zsh lua liblua
 
 echo "\n\n"
 echo "----------> Configuring zsh"
@@ -20,8 +20,20 @@ ln -s ./dotfiles/.zshrc ./.zshrc
 ln -s ./dotfiles/.vimrc ./.vimrc
 
 echo "\n\n"
+echo "----------> Setup .vim"
+mkdir .vim
+mkdir .vim/tmp
+mkdir .vim/tmp/swap
+mkdir .vim/tmp/backup
+mkdir .vim/tmp/undo
+mkdir .vim/bundle
+git clone git@github.com:Shougo/neobundle.vim.git ./.vim/bundle/neobundle.vim
+
+echo "\n\n"
+echo "----------> Setup vimenv"
+git clone git@github.com:raa0121/vimenv.git .vimenv
+
+echo "\n\n"
 echo "----------> Change shell"
 chsh -s /usr/bin/zsh
 source ./.zshrc
-
-
